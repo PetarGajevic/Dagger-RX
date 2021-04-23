54e9fa5d4a15e6a9cc652b4e8a3089fcfc796c3a
Petar Gajevic <petar.gajevic@wind.co.me>
2021-04-23 08:45:07 +0200

    initial commit

---
+++ app/src/main/java/com/e/rxjavatest/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.e.rxjavatest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.e.dagger.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
